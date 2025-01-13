28b7f3a0e8264ecc2404ac17640688abb610c4c8
fxsegura <fxsegura.dev@gmail.com>
2025-01-13 09:04:05 -0500

    Custom screen size calcukations

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,3 +21,14 @@ export const navLinks = [
     },
   ];
   
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -6, 0] : [0.25, -9, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -7, 0] : isTablet ? [7, -5, 0] : [10, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [6, 3, 0] : [10, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-14, 7.5, 0] : isTablet ? [-12, 6, 0] : [-20, 6, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-7, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+  };
+ 
