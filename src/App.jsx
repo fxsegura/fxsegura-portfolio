6411896a527bab304ba52dcb3616694fc844d8f5
fxsegura <fxsegura.dev@gmail.com>
2025-01-13 17:09:17 -0500

    Completed About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const App = () => {
   return (
     <main className="max-w-screen mx-auto">
       <Navbar/>  
       <Hero/>  
-      
+      <About/>
     </main>
   );
 }
