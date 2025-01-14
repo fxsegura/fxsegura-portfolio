08e328b1e181def1fc76142d602168679e0c15f6
fxsegura <fxsegura.dev@gmail.com>
2025-01-14 12:23:21 -0500

    Removed contact form background. Doesn't work well on all resolutions.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -64,8 +64,8 @@ const Contact = () => {
         <section id="contact" className="c-space my-20">
             {alert.show && <Alert {...alert} />}
             <div className="relative min-h-screen flex items-center justify-center flex-col">
-                {!isMobile && 
-                <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen"/>}
+                {/* {!isMobile && 
+                <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen"/>} */}
                 <div className="contact-container">
                     <h3 className="head-text">Let's Talk</h3>
                     <p className="text-lg text-white-600 mt-3">
