51d1c7d41efd5befdd01ebd328f5ec9602ae73ad
fxsegura <fxsegura.dev@gmail.com>
2025-01-13 21:16:58 -0500

    Completed projects section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
     return (
-        <section className="min-h-screen w-full flex flex-col relative">
+        <section id="home" className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-uto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Francisco <span className="waving-hand">👋</span></p>
                 <p className="hero_tag text-gray_gradient">Building Products and Brands</p>
@@ -44,7 +44,7 @@ const Hero = () => {
                 </Canvas>
             </div>
             <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-                <a href="#contact" className="scroll-down w-fit">
+                <a href="#about" className="scroll-down w-fit">
                     <Button name="Let's work together!" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
                 </a>
             </div>
